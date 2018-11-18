c977316c294560f5a7ef8e64678956c1dbd2a657
Ankit Kumar Yadav <30716750+ankravkk@users.noreply.github.com>
2018-11-18 18:35:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,233 @@
+
+<!doctype html>
+<html>
+<head>
+
+    <title>Hackethon | Buy and Sell old/used Books and Study Material </title>
+<meta property="og:title" content="Full2Study | Buy and Sell old/used Books and Study Material ">
+<meta property="og:image" content="https://full2study.com/img/logo.jpg
+">
+<meta name="description" content="Full2Study is an online portal to buy and sell old/used books, notes and other study material for free and no intermediator.
+"/>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="theme-color" content="#009933">
+<link rel="icon" type="image/png" href="https://full2study.com/img/favicon.png">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124514759-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-124514759-1');
+</script>
+<!--  Font Awesome  -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Bootstrap core CSS -->
+<link href="https://full2study.com/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://full2study.com/css/mdb.min.css" rel="stylesheet">
+<style>	
+body {
+  background-color:#99ccff;
+}
+main {
+	padding-top: 80px;
+}
+</style>
+
+<link rel="manifest" href="/manifest.json" />
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "8e9df9f4-6ef8-4606-a167-7543ec272ce4",
+      autoRegister: true,
+      notifyButton: {
+        enable: true,
+        position: 'bottom-left',
+      },
+      welcomeNotification: {
+        disable: true
+      },
+    });
+  });
+</script>
+
+</head>
+<body>
+	<header>
+
+    	<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar indigo">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <strong>Buy and sell books </strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7"
+          aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">
+                        Sell Now
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">
+                        About Us
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" target="_blank">
+                        Contact Us
+                    </a>
+                </li>
+                                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="fa fa-university"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink-5">
+                                                                                    <a class="dropdown-item" href="https://full2study.com/college/1">College 1</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/2">College 2</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/3">College 3</a>
+                                                  <!--            <a class="dropdown-item" href="https://full2study.com/college/4">Galgotia University</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/9">GNIOT</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/11">Govt.Polytechnic Katihar</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/6">IMS Engineering College</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/5">JSSATE Noida</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/12">KCE Bihar</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/7">KIET Ghaziabad</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/8">NIET Greater Noida</a>
+                                                            <a class="dropdown-item" href="https://full2study.com/college/10">Skyline Greater Noida</a>-->
+                                                                        </div>
+                </li>
+                            </ul>
+            <ul class="navbar-nav nav-flex-icons">
+                                    <li class="nav-item">
+                        <a class="nav-link" href="login.html">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                            </ul>
+                    </div>
+    </div>
+</nav>
+
+
+    </header>
+    <main>
+    	
+        <div class="container">
+    <section>
+                <form action="#" method="GET">
+    <div class="input-group input-group-lg hoverable" style="background-color:#4B515D;padding-right:20px;padding-left:20px;padding-top:20px">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm"><i class="fa fa-search"></i></span>
+            </div>
+            <input type="text" name="book" value="" class="form-control mb-4" placeholder="Search Book" required autocomplete="off">
+            <div class="input-group-append">
+                <button type="submit" class="btn btn-primary waves-effect m-0" type="submit">Search</button>
+            </div>
+    </div>
+</form>        <br>
+                                                        <div class="card-deck">
+                                                        <a
+                                                                        href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                                                             <br><br>
+                                <strong>Engineering Stationary</strong>
+                            </div>
+                                            </a>
+                                                                                                                                                                                                                    <a
+                                                                        href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                                                           <br><br>
+                                <strong>1st year (Common)</strong>
+                            </div>
+                                            </a>                                                                                                                                                                                                 
+                                                                                                                                                                                                                    <a
+                                                                        href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                             
+                                <br><br>
+                                <strong>Electronics &amp; Communication</strong>
+                            </div>
+                                            </a>
+                                                                                                                                                            </div><br><div class="scard-deck">
+                                                                                                        
+                                                                    <a    href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                            
+                                <br><br>
+                                <strong>Electrical</strong>
+                            </div>
+                                            </a>
+                                                                                                                                                                                                                    <a
+                                                                        href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                            
+                                <br><br>
+                                <strong>Mechanical</strong>
+                            </div>
+                                            </a>
+                                                                                                                                                                                                                    <a
+                                                                        href="#"
+                                             
+                                            class="card hoverable"
+                                        >
+                                                    <div class="card-body text-center">
+                            
+                                <br><br>
+                                <strong>Civil</strong>
+                            </div>
+                                            </a>
+                                                                                                                                    </div>
+    </section>
+    <!--
+    <hr class="my-5">
+    <section id="products" class="wow fadeIn" data-wow-delay="0.3s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.3s;">
+        <center>
+            <div class="embed-responsive embed-responsive-16by9" style="max-width:600px">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wTeniMpT3Qc?rel=0" allowfullscreen></iframe>
+            </div>
+        </center>
+    </section>
+-->
+    <hr class="my-5">
+<section id="products" class="text-center wow fadeIn" data-wow-delay="0.3s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.3s;">
+    <h1 class="font-weight-bold text-center h1">Want to sell your books ?</h1>
+    <p class="text-center grey-text mx-auto w-responsive lead">It is free and easy process to sell your books, just add details and buyers will contact you directly.</p>
+    <a href="#" class="btn blue-gradient text-white btn-lg active my-3" role="button">Sell Now</a>
+</section></div>
+
+    </main>
+
+    <img src="https://full2study.com/img/foot.png" width="100%" alt="Full2Study is an online marketplace for study materials">
+
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	       <link rel="stylesheet" href="login.css" media="all"/>
+</head>
+<body>
+<form action="register.php" method="post" enctype="multipart/form-data" class="register">
+									        <table align="center" width="740">
+											      <tr>
+												      
+													   <td colspan="2" align="center"><h2>Register here...</h2></td>
+												  </tr>
+												  <tr>
+												       <td><b>Full Name:</b></td>
+													   <td><input type="text" name="c_name"/></td>
+												  </tr>
+												   <tr>
+												       <td><b> Email Id:</b></td>
+													   <td><input type="text" name="c_email"/></td>
+												  </tr>
+												   <tr>
+												       <td><b>Phone</b></td>
+													   <td><input type="text" name="c_pass"/></td>
+												  </tr>
+												   <tr>
+												       <td><b>College Name</b></td>
+													   <td><input type="text" name="c_country"/></td>
+												  </tr>
+												   <tr>
+												       <td><b>Password:</b></td>
+													   <td><input type="password" name="c_city"/></td>
+												  </tr>
+												  <tr>
+												       <td><b>Confirm Password:</b></td>
+													   <td><input type="password" name="c_contact"/></td>
+												  </tr>
+												 
+												  <tr>
+												       <td colspan="2" align="center"><input type="submit" name="register" value="Register"/></td>
+													  
+												  </tr>
+											 </table>
+									  </form>
+								     </body>
+							</html>
